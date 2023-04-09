cb3c23780b56a2e3825ef4023af7cd2598154947
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-04-10 00:12:40 +0200

    added -- Navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,9 +3,103 @@ import { Link } from "react-router-dom";
 import { navLinks } from "../../constants";
 import { logo, menu, close } from "../../assets";
 import classes from "./styles.module.scss";
+import clsx from "clsx";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <nav
+      className={clsx(
+        classes.paddingX,
+        classes.wrapper,
+        scrolled ? classes.scrolled : classes.unscrolled
+      )}
+    >
+      <div className={classes.navContainer}>
+        <Link
+          to="/"
+          className={classes.rightNavContainer}
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img className={classes.favIcon} src={logo} alt="logo" />
+          <p className={classes.title}>
+            Adrian &nbsp;
+            <span className={classes.description}>| JavaScript Mastery</span>
+          </p>
+        </Link>
+
+        <ul className={classes.largeMenuContainer}>
+          {navLinks.map((nav) => (
+            <li
+              className={clsx(
+                classes.menuItems,
+                active == nav.title ? classes.activeMenu : classes.deactiveMenu
+              )}
+              key={nav.id}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className={classes.moblieMenuContainer}>
+          <img
+            className={classes.menuIcon}
+            src={toggle ? close : menu}
+            alt="menu"
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div
+            className={clsx(classes.mobileMenuItems, {
+              [classes.activeMobileMenu]: toggle,
+              [classes.deactiveMobileMenu]: !toggle,
+            })}
+          >
+            <ul className={classes.container}>
+              {navLinks.map((nav) => (
+                <li
+                  className={clsx(
+                    classes.containerItem,
+                    active == nav.title
+                      ? classes.activeMenu
+                      : classes.deactiveMenu
+                  )}
+                  key={nav.id}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(nav.title);
+                  }}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
