eb1989174da33b8f7241d8c4f787baa245516307
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-04-10 00:11:05 +0200

    ready -- App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+import classes from "./styles.module.scss";
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Navbar />
+      <div className={classes.wrapper}>
+        <div className={classes.navbarAndHeroSection}>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className={classes.contactSection}>
+          <Contact />
+          <StarsCanvas />
+        </div>
       </div>
     </BrowserRouter>
   );
