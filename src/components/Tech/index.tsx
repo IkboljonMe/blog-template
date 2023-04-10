260c93c55fea374a0b31f20ae44f4bf786648d28
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-04-10 18:52:07 +0200

    updaeted -- Tech parts

--- src/components/Tech/index.tsx
+++ src/components/Tech/index.tsx
@@ -1,7 +1,17 @@
+import { BallCanvas } from "../canvas";
+import { SectionWrapper } from "../../hoc";
+import { technologies } from "../../constants";
 import classes from "./styles.module.scss";
-
 const Tech = () => {
-  return <div>Tech</div>;
+  return (
+    <div className={classes.wrapper}>
+      {technologies.map((technology) => (
+        <div key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
-export default Tech;
+export default SectionWrapper(Tech, "");
