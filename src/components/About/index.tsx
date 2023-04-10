bff9066f8fb14735b9c027d0d4dc20434aa450fe
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-04-10 18:50:56 +0200

    updated --About.tsx

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,6 +1,72 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { services } from "../../constants";
+import { SectionWrapper } from "../../hoc";
+import { fadeIn, textVariant } from "../../utils/motion";
 import classes from "./styles.module.scss";
 
-const About = () => {
-  return <div>About</div>;
+interface ServiceCardProps {
+  index: number;
+  title: string;
+  icon: string;
+}
+const ServiceCard: React.FC<ServiceCardProps> = ({ index, title, icon }) => {
+  return (
+    <Tilt
+      options={{
+        max: 45,
+        scale: 1,
+        speed: 450,
+      }}
+    >
+      <motion.div
+        variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+        className={classes.cardWrapper}
+      >
+        <div className={classes.card}>
+          <img
+            src={icon}
+            alt="web-development"
+            className="w-16 h-16 object-contain"
+          />
+
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
 };
-export default About;
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={classes.sectionSubText}>Introduction</p>
+        <h2 className={classes.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className={classes.description}
+      >
+        I'm a skilled software developer with experience in TypeScript and
+        JavaScript, and expertise in frameworks like React, Node.js, and
+        Three.js. I'm a quick learner and collaborate closely with clients to
+        create efficient, scalable, and user-friendly solutions that solve
+        real-world problems. Let's work together to bring your ideas to life!
+      </motion.p>
+
+      <div className={classes.servicesContainer}>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
