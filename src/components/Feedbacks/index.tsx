7fcdea078a610e37aa1bdb99f297c479e9575787
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-04-13 01:21:15 +0200

    new FeedBack card

--- src/components/Feedbacks/index.tsx
+++ src/components/Feedbacks/index.tsx
@@ -1,41 +1,9 @@
-import React from "react";
 import { motion } from "framer-motion";
 import { SectionWrapper } from "../../hoc";
-import { fadeIn, textVariant } from "../../utils/motion";
+import { textVariant } from "../../utils/motion";
 import { testimonials } from "../../constants";
-import { Testimonial } from "../../constants/index.types";
 import classes from "./styles.module.scss";
-interface FeedbackCardProps {
-  feedback: Testimonial;
-  index: number;
-}
-const FeedbackCard: React.FC<FeedbackCardProps> = ({ feedback, index }) => {
-  const { testimonial, name, designation, company, image, surname } = feedback;
-  return (
-    <motion.div
-      variants={fadeIn("", "spring", index * 0.5, 0.75)}
-      className={classes.feedBackCard}
-    >
-      <p>"</p>
-      <div className={classes.description}>
-        <p>{testimonial}</p>
-        <div>
-          <div>
-            <div>
-              <span>@</span>
-              <p>{name}</p>
-              <p>{surname}</p>
-            </div>
-            <p>
-              {designation} of {company}
-            </p>
-          </div>
-          <img src={image} alt={`feedback_by-${name}`} />
-        </div>
-      </div>
-    </motion.div>
-  );
-};
+import FeedbackCard from "./FeedbackCard";
 
 const Feedbacks = () => {
   return (
