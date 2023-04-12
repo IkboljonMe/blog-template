55a29383caa3c8af1377f1c83d203ca9f84b6771
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-04-13 01:20:02 +0200

    works section renamed to projects

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 import { github } from "../../assets";
-import { SectionWrapper } from "../../hoc";
-import { projects } from "../../constants";
-import { fadeIn, textVariant } from "../../utils/motion";
+import { fadeIn } from "../../utils/motion";
 import { Project } from "../../constants/index.types";
 import classes from "./styles.module.scss";
 import clsx from "clsx";
-
 interface ProjectCardProps {
   project: Project;
   index: number;
@@ -26,11 +23,7 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project, index }) => {
         className={classes.cardTilt}
       >
         <div className={classes.imageContainer}>
-          <img
-            src={image}
-            alt="project_image"
-            className="w-full h-full object-cover rounded-2xl"
-          />
+          <img src={image} alt="project_image" />
 
           <div className={classes.githubIconBtn}>
             <div onClick={() => window.open(source_code_link, "_blank")}>
@@ -59,32 +52,4 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project, index }) => {
     </motion.div>
   );
 };
-
-const Works = () => {
-  return (
-    <div className={classes.wrapper}>
-      <motion.div variants={textVariant()}>
-        <p className={classes.sectionSubText}>My work</p>
-        <h2 className={classes.sectionHeadText}>Projects.</h2>
-      </motion.div>
-
-      <div className={classes.topWorksSection}>
-        <motion.p variants={fadeIn("", "", 0.1, 1)}>
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
-        </motion.p>
-      </div>
-
-      <div className={classes.projectsSection}>
-        {projects.map((project: Project, index: number) => (
-          <ProjectCard key={index} project={project} index={index} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default SectionWrapper(Works, "");
+export default ProjectCard;
--- src/components/index.ts
+++ src/components/index.ts
@@ -8,7 +8,7 @@ import Hero from "./Hero";
 import Navbar from "./Navbar";
 import About from "./About";
 import Experience from "./Experience";
-import Works from "./Works";
+import Works from "./Projects";
 import Feedbacks from "./Feedbacks";
 import Contact from "./Contact";
 import CanvasLoader from "./Loader";
