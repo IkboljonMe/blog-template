8a510244a3155bb9d4d4f363e40b250f107c18bd
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-04-23 14:34:26 +0200

    clean-up | projects data |

---
+++ src/constants/projects.ts
@@ -0,0 +1,64 @@
+import { Project } from "./index.types";
+import { guess, portfolio, eCommerce } from "../assets";
+const projects: Project[] = [
+  {
+    name: "Guees game",
+    description:
+      "Let's play Guess my number game. Guess number between 0 to 20",
+    tags: [
+      {
+        name: "vanilla-javascript",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css",
+        color: "green-text-gradient",
+      },
+    ],
+    image: guess,
+    source_code_link: "https://github.com/IkboljonMe/guess-number-web",
+  },
+  {
+    name: "Portfolio",
+    description:
+      "Beatifull portfolio website with React+Nextjs. Build your own",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "redux",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: portfolio,
+    source_code_link: "https://github.com/IkboljonMe/commerce-project-web",
+  },
+  {
+    name: "E-Commerce",
+    description: "UzShop E-commerce shop with React. Fully responsive",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: eCommerce,
+    source_code_link: "https://github.com/IkboljonMe/commerce-project-web",
+  },
+];
+
+export default projects;
