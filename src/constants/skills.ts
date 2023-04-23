8207abba4a2c5df70fbb2ac62e43c842f8539db7
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-04-23 15:12:26 +0200

    data added

--- src/constants/skills.ts
+++ src/constants/skills.ts
@@ -43,13 +43,29 @@ const skills: Skill[] = [
           { name: "News", link: "https://myblog.com" },
         ],
       },
+      {
+        name: "MongoDb",
+        icon: skillPrisma,
+        projects: [
+          { name: "UzShop", link: "https://auction.com" },
+          { name: "News", link: "https://myblog.com" },
+        ],
+      },
+      {
+        name: "Firebase",
+        icon: skillPrisma,
+        projects: [
+          { name: "UzShop", link: "https://auction.com" },
+          { name: "News", link: "https://myblog.com" },
+        ],
+      },
     ],
   },
   {
     name: "Front-end developer",
     data: [
       {
-        name: "Nextjs",
+        name: "HTML|CSS|JS",
         icon: skillNextjs,
         projects: [
           { name: "Auction", link: "https://auction.com" },
@@ -62,6 +78,75 @@ const skills: Skill[] = [
         projects: [
           { name: "Auction", link: "https://auction.com" },
           { name: "Portfolio", link: "https://myblog.com" },
+        ],
+      },
+      {
+        name: "Nextjs",
+        icon: skillNextjs,
+        projects: [
+          { name: "Auction", link: "https://auction.com" },
+          { name: "App", link: "https://myblog.com" },
+        ],
+      },
+      {
+        name: "Material UI",
+        icon: skillReactjs,
+        projects: [
+          { name: "Auction", link: "https://auction.com" },
+          { name: "Portfolio", link: "https://myblog.com" },
+        ],
+      },
+      {
+        name: "Bootstrap",
+        icon: skillReactjs,
+        projects: [
+          { name: "Auction", link: "https://auction.com" },
+          { name: "Portfolio", link: "https://myblog.com" },
+        ],
+      },
+      {
+        name: "SASS",
+        icon: skillPrisma,
+        projects: [
+          { name: "UzShop", link: "https://auction.com" },
+          { name: "News", link: "https://myblog.com" },
+        ],
+      },
+    ],
+  },
+  {
+    name: "Additional",
+    data: [
+      {
+        name: "Docker",
+        icon: skillNextjs,
+        projects: [
+          { name: "Auction", link: "https://auction.com" },
+          { name: "App", link: "https://myblog.com" },
+        ],
+      },
+      {
+        name: "Github|Gitlab CI/CD",
+        icon: skillNextjs,
+        projects: [
+          { name: "Auction", link: "https://auction.com" },
+          { name: "App", link: "https://myblog.com" },
+        ],
+      },
+      {
+        name: "Design Patterns",
+        icon: skillNextjs,
+        projects: [
+          { name: "Auction", link: "https://auction.com" },
+          { name: "App", link: "https://myblog.com" },
+        ],
+      },
+      {
+        name: "Solid Princeples",
+        icon: skillNextjs,
+        projects: [
+          { name: "Auction", link: "https://auction.com" },
+          { name: "App", link: "https://myblog.com" },
         ],
       },
     ],
