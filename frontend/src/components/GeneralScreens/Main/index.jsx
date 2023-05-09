d28c78e9810ba7ac0a8aad8c24743ca05ef4c531
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-05-10 01:30:02 +0200

    img rotation func

--- frontend/src/components/GeneralScreens/Main/index.jsx
+++ frontend/src/components/GeneralScreens/Main/index.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../Navbar";
 import classes from "./styles.module.scss";
 import { BsGithub, BsYoutube, BsTelegram, BsLinkedin } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import clsx from "clsx";
 
 const Main = () => {
+  const [isRotating, setIsRotating] = useState(false);
   return (
     <div className={classes.wrapper}>
       <Navbar />
       <section className={classes.imageContainer}>
-        <img src="https://avatars.githubusercontent.com/ikboljonme" alt="" />
+        <img
+          onClick={() => {
+            setIsRotating(!isRotating);
+          }}
+          className={clsx(isRotating && classes.imgRotation)}
+          src="https://avatars.githubusercontent.com/ikboljonme"
+          alt=""
+        />
         <div>
           <h1>Ikboljon Abdurasulov</h1>
           <p>Software Engineer</p>
