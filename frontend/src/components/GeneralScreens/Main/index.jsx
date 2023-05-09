a62086f8aa497910ae77b21f5d6668d2c49f5067
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-05-10 01:21:07 +0200

    main page finished

--- frontend/src/components/GeneralScreens/Main/index.jsx
+++ frontend/src/components/GeneralScreens/Main/index.jsx
@@ -1,11 +1,41 @@
 import React from "react";
 import Navbar from "../Navbar";
-// import classes from "./styles.module.scss";
+import classes from "./styles.module.scss";
+import { BsGithub, BsYoutube, BsTelegram, BsLinkedin } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Main = () => {
   return (
-    <div>
+    <div className={classes.wrapper}>
       <Navbar />
+      <section className={classes.imageContainer}>
+        <img src="https://avatars.githubusercontent.com/ikboljonme" alt="" />
+        <div>
+          <h1>Ikboljon Abdurasulov</h1>
+          <p>Software Engineer</p>
+          <div>
+            <Link to="https://www.linkedin.com/in/ikboljonme/">
+              <BsLinkedin size="3x" />
+            </Link>
+            <Link to="">
+              <BsGithub size="3x" />
+            </Link>
+            <Link to="">
+              <BsTelegram size="3x" />
+            </Link>
+            <Link to="/">
+              <BsYoutube size="3x" />
+            </Link>
+          </div>
+        </div>
+      </section>
+      <section className={classes.buttonsContainer}>
+        <p> Yeey. It's IkboljonMe. Check my portfolio or thouths below</p>
+        <div>
+          <button className={classes.blog}>Read blog</button>
+          <button className={classes.portfolio}>Portfolio</button>
+        </div>
+      </section>
     </div>
   );
 };
