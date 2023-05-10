5a4cfae35875e7d67ee7d8f6ce654e2462f09ab8
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-05-10 22:03:37 +0200

    done menu

--- frontend/src/components/GeneralScreens/MainPage/Navbar/index.jsx
+++ frontend/src/components/GeneralScreens/MainPage/Navbar/index.jsx
@@ -4,45 +4,57 @@ import classes from "./styles.module.scss";
 import { motion } from "framer-motion";
 import { HiMenu } from "react-icons/hi";
 import { IoClose } from "react-icons/io5";
-const navbarLinks = ["Home", "Blog", "Portfolio", "Talks"];
+const navbarLinks = ["Home", "Blog", "Portfolio", "Projects"];
 
 const Navbar = () => {
   const [menu, setMenu] = useState(false);
   return (
-    <nav className={classes.navbar}>
-      <Link to="/">
-        <motion.p whileHover={{ scale: 1.05 }}>Ikboljon's Blog</motion.p>
-      </Link>
-      <ul>
-        {navbarLinks.map((link, index) => (
-          <motion.li whileHover={{ scale: 1.05 }}>
-            <Link key={index} to={link.toLowerCase}>
-              {link}
-            </Link>
-          </motion.li>
-        ))}
-      </ul>
-      <menu>
-        {menu ? (
-          <i
-            onClick={() => {
-              setMenu(!menu);
-            }}
-          >
-            <HiMenu size="25" />
-          </i>
-        ) : (
-          <i>
-            <IoClose
-              size="25"
+    <>
+      <nav className={classes.navbar}>
+        <Link to="/">
+          <motion.p whileHover={{ scale: 1.05 }}>Ikboljon's Blog</motion.p>
+        </Link>
+        <ul>
+          {navbarLinks.map((link, index) => (
+            <motion.li whileHover={{ scale: 1.05 }}>
+              <Link key={index} to={"/" + link.toLowerCase()}>
+                {link}
+              </Link>
+            </motion.li>
+          ))}
+        </ul>
+        <menu>
+          {menu ? (
+            <i
               onClick={() => {
                 setMenu(!menu);
               }}
-            />
-          </i>
+            >
+              <HiMenu size="25" />
+            </i>
+          ) : (
+            <i>
+              <IoClose
+                size="25"
+                onClick={() => {
+                  setMenu(!menu);
+                }}
+              />
+            </i>
+          )}
+        </menu>
+        {!menu && (
+          <section className={classes.menuSection}>
+            <div></div>
+            {navbarLinks.map((link, index) => (
+              <p>
+                <Link to={"/" + link.toLowerCase()}>{link}</Link>
+              </p>
+            ))}
+          </section>
         )}
-      </menu>
-    </nav>
+      </nav>
+    </>
   );
 };
 
