2654a555ac6f311b17da6b22da73e6b5a96a4495
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-05-15 00:49:36 +0200

    dleted unnecessary files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import classes from "./App.module.scss";
-import MainPage from "./components/GeneralScreens/MainPage/index";
+import MainPage from "./components/Home/MainPage";
 function App() {
   return (
     <div className={classes.app}>
