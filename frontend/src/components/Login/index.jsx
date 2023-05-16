9233b77d69752d795da18b873367a958191256e0
Oybek <saminovo150947@gmail.com>
2023-05-16 09:32:58 +0500

    changed Login/index.jsx

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Login = () => {
   return <div>
   <h1>Login form </h1>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, inventore.</p>
+  <h2>Start login page</h2>
     </div>;
 };
 
