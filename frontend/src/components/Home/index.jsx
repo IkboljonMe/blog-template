a0582f9c883434135cf74439535b8491891e410d
Oybek <saminovo150947@gmail.com>
2023-05-16 22:26:04 +0500

    create login Forma

--- frontend/src/components/Home/index.jsx
+++ frontend/src/components/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Login from "../Login"
 
 import classes from "./styles.module.scss";
 import MainSection from "./MainSection";
@@ -9,9 +10,11 @@ const Home = () => {
   return (
     <div>
       <div className={classes.wrapper}>
+        <Login/>
         <Navbar />
         <MainSection />
         <Footer />
+
       </div>
     </div>
   );
--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -1,10 +1,131 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import classes from "./login.module.scss";
+const Form = () => {
+  const [email, setEmail] = useState("");
+  const [password, setpassword] = useState("");
+  const [emailDirty, setEmailDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [emailError, setEmailError] = useState("Please fill in the space");
+  const [passwordError, setPasswordError] = useState(
+    "The password was entered incorrectly"
+  );
+  const [formValid, setFormValid] = useState(false);
+  const [loginError, setLoginError] = useState(
+    "Iltimos malumotlaringizni kiriting!!!"
+  );
+  
 
-const Login = () => {
-  return <div>
-  <h1>Login form </h1>
-  <h2>Start login page</h2>
-    </div>;
+  useEffect(() => {
+    if (emailError || passwordError) {
+      setFormValid(false);
+    } else {
+      setFormValid(true);
+    }
+  }, [emailError, passwordError]);
+
+  const emailHandler = (e) => {
+    setEmail(e.target.value);
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (!re.test(String(email).toLowerCase())) {
+      setEmailError("You have entered an email error");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const passwordHandler = (e) => {
+    setpassword(e.target.value);
+    const passwordRegex =
+      /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    if (!passwordRegex.test(String(password).toLowerCase())) {
+      setPasswordError("parol raqam, belgi va harflardan iborat bolishi kerak");
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case "email":
+        setEmailDirty(true);
+        break;
+      case "password":
+        setPasswordDirty(true);
+        break;
+    }
+  };
+
+  // OpenModal
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <div className={classes.form}>
+      <div className={classes.main}>
+        <div className={classes.loginBox}>
+          <p>
+            Don't have an account?{" "}
+            <a href="#" className="a2">
+              Sign up!
+            </a>
+          </p>
+          {email && password && (
+            <div style={{ color: "red" }}>{loginError}</div>
+          )}
+
+          <h1>Login to your account</h1>
+          <p>Piease Login Your account,Thank you</p>
+
+          <form className={classes.formInput}>
+            {emailDirty && emailError && (
+              <div style={{ color: "red" }}>{emailError}</div>
+            )}
+
+            <div>
+              <input
+                onChange={(e) => emailHandler(e)}
+                onBlur={(e) => blurHandler(e)}
+                required=""
+                value={email}
+                name="email"
+                type="text"
+                placeholder="john@ gmail.com"
+              />
+              <label>Email</label>
+            </div>
+
+            <div>
+              <input
+                onChange={(e) => passwordHandler(e)}
+                onBlur={(e) => blurHandler(e)}
+                required=""
+                value={password}
+                name="password"
+                type="password"
+                placeholder="6-strong character"
+              />
+              <label>Password</label>
+
+              {passwordDirty && passwordError && (
+                <div style={{ color: "red" }}>{passwordError}</div>
+              )}
+            </div>
+            <div className={classes.forgotPassword}>
+              <button onClick={() => setOpenModal(true)}>
+                Forgot Password?
+              </button>
+            </div>
+            <button disabled={!formValid} type="submit" className={classes.loginBtn}>
+              Login
+            </button>
+          </form>
+        </div>
+
+        <div className={classes.emptyPlace}></div>
+      </div>
+    </div>
+  );
 };
 
-export default Login;
+export default Form;
