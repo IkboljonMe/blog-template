f8845271a316dbb97453c7aff84277a0ed1ab643
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-05-17 02:27:27 +0200

    login & logout button navbar

--- frontend/src/components/Home/Navbar/index.jsx
+++ frontend/src/components/Home/Navbar/index.jsx
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import classes from "./styles.module.scss";
 import { motion } from "framer-motion";
 import { HiMenu } from "react-icons/hi";
 import { IoClose } from "react-icons/io5";
-const navbarLinks = ["Home", "Blog", "Portfolio", "Projects", "Login"];
+import { useNavigate } from "react-router-dom";
+const navbarLinks = ["Home", "Blog", "Portfolio", "Projects"];
 
 const Navbar = () => {
+  const bool = localStorage.getItem("authToken") ? true : false;
   const [menu, setMenu] = useState(false);
+  const [auth, setAuth] = useState(bool);
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  useEffect(() => {
+    setAuth(bool);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1600);
+  }, [bool]);
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    navigate("/");
+  };
   return (
     <>
       <nav className={classes.navbar}>
@@ -18,15 +33,19 @@ const Navbar = () => {
         </Link>
         <ul>
           {navbarLinks.map((link, index) => (
-            <motion.li whileHover={{ scale: 1.05 }}>
+            <motion.li key={index} whileHover={{ scale: 1.05 }}>
               <Link
-                key={index}
                 to={link !== "Login" ? "/" + link.toLowerCase() : "/auth/login"}
               >
                 {link}
               </Link>
             </motion.li>
           ))}
+          <motion.li whileHover={{ scale: 1.05 }}>
+            <Link onClick={auth && handleLogout} to={!auth && "/auth/login"}>
+              {auth ? "Log out" : "Login"}
+            </Link>
+          </motion.li>
         </ul>
         <menu>
           {menu ? (
