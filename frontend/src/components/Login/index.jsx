c86faa8bb638dc67413917323fffc3d2a920ca66
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-05-17 02:08:55 +0200

    login func

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -1,62 +1,32 @@
 import React, { useState, useEffect } from "react";
 import classes from "./login.module.scss";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 const Form = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [emailDirty, setEmailDirty] = useState(false);
-  const [passwordDirty, setPasswordDirty] = useState(false);
-  const [emailError, setEmailError] = useState("Please fill in the space");
-  const [passwordError, setPasswordError] = useState(
-    "The password was entered incorrectly"
-  );
-  const [formValid, setFormValid] = useState(false);
-  const [loginError, setLoginError] = useState(
-    "Iltimos malumotlaringizni kiriting!!!"
-  );
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-  useEffect(() => {
-    if (emailError || passwordError) {
-      setFormValid(false);
-    } else {
-      setFormValid(true);
-    }
-  }, [emailError, passwordError]);
+  const loginHandler = async (e) => {
+    e.preventDefault();
 
-  const emailHandler = (e) => {
-    setEmail(e.target.value);
-    const re =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    try {
+      const { data } = await axios.post("/auth/login", { email, password });
+      localStorage.setItem("authToken", data.token);
 
-    if (!re.test(String(email).toLowerCase())) {
-      setEmailError("You have entered an email error");
-    } else {
-      setEmailError("");
+      setTimeout(() => {
+        navigate("/");
+      }, 1800);
+    } catch (error) {
+      setError(error.response.data.error);
+      setTimeout(() => {
+        setError("");
+      }, 4500);
     }
   };
-
-  const passwordHandler = (e) => {
-    setPassword(e.target.value);
-    const passwordRegex =
-      /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-    if (!passwordRegex.test(String(password).toLowerCase())) {
-      setPasswordError("parol raqam, belgi va harflardan iborat bolishi kerak");
-    } else {
-      setPasswordError("");
-    }
-  };
-
-  const blurHandler = (e) => {
-    switch (e.target.name) {
-      case "email":
-        setEmailDirty(true);
-        break;
-      case "password":
-        setPasswordDirty(true);
-        break;
-    }
-  };
-
+  console.log(error);
   return (
     <div className={classes.form}>
       <div className={classes.main}>
@@ -64,20 +34,14 @@ const Form = () => {
           <p>
             Don't have an account? <Link to="/auth/register">Sign up!</Link>
           </p>
-          {email && password && (
-            <div style={{ color: "red" }}>{loginError}</div>
-          )}
+
           <h1>Login to your account</h1>
           <p>Piease Login Your account,Thank you</p>
-          <form className={classes.formInput}>
-            {emailDirty && emailError && (
-              <div style={{ color: "red" }}>{emailError}</div>
+          <form onSubmit={loginHandler} className={classes.formInput}>
             )}
-
             <div>
               <input
-                onChange={(e) => emailHandler(e)}
-                onBlur={(e) => blurHandler(e)}
+                onChange={(e) => setEmail(e.target.value)}
                 required=""
                 value={email}
                 name="email"
@@ -86,11 +50,9 @@ const Form = () => {
               />
               <label>Email</label>
             </div>
-
             <div>
               <input
-                onChange={(e) => passwordHandler(e)}
-                onBlur={(e) => blurHandler(e)}
+                onChange={(e) => setPassword(e.target.value)}
                 required=""
                 value={password}
                 name="password"
@@ -98,21 +60,13 @@ const Form = () => {
                 placeholder="6-strong character"
               />
               <label>Password</label>
-
-              {passwordDirty && passwordError && (
-                <div style={{ color: "red" }}>{passwordError}</div>
-              )}
             </div>
             <div className={classes.forgotPassword}>
               <Link to="/register">
                 <button>Forgot Password?</button>
               </Link>
             </div>
-            <button
-              disabled={!formValid}
-              type="submit"
-              className={classes.loginBtn}
-            >
+            <button type="submit" className={classes.loginBtn}>
               Login
             </button>
           </form>
