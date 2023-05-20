53939c758b7c576917bd4a259b399a2f88690ffb
Ikboljon Abdurasulov <ikboljon@radcode.co>
2023-05-20 11:10:46 +0200

    clean up 3d

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,6 +1,3 @@
-import { motion } from "framer-motion";
-import { ComputersCanvas } from "../canvas";
-import { ErrorBoundary } from "react-error-boundary";
 import classes from "./styles.module.scss";
 import { BsGithub, BsLinkedin, BsTelegram, BsYoutube } from "react-icons/bs";
 
@@ -18,25 +15,6 @@ const Hero = () => {
           <BsTelegram />
           <BsYoutube />
         </div>
-      </div>
-
-      <ErrorBoundary fallback={<div>Something went wrong</div>}>
-        <ComputersCanvas />
-      </ErrorBoundary>
-
-      <div className={classes.bottomBtnContainer}>
-        <a href="#about" className={classes.btnDown}>
-          <motion.div
-            animate={{
-              y: [0, 24, 0],
-            }}
-            transition={{
-              duration: 1.5,
-              repeat: Infinity,
-              repeatType: "loop",
-            }}
-          />
-        </a>
       </div>
     </section>
   );
