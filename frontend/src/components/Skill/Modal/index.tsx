6ba2c6a445bff2c1062524b3a23cc10561e0d2f2
Oybek <saminovo150947@gmail.com>
2023-06-02 10:52:13 +0500

    changed tsx in modal

--- frontend/src/components/Skill/Modal/index.tsx
+++ frontend/src/components/Skill/Modal/index.tsx
@@ -1,11 +1,18 @@
 import classes from "./styles.module.scss";
 import clsx from "clsx";
 import React from "react";
+import { FaTimes } from "react-icons/fa";
+import  {react}  from "../../../assets/modalImages";
+import {HiArrowTopRightOnSquare} from 'react-icons/hi2'
+import {Modal} from "../../../constants/index.types"
+import { HiCode } from "react-icons/hi";
+
 interface ModalProps {
+  modals: Modal;
   isModalOpen: boolean;
   setIsModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
-const Modal: React.FC<ModalProps> = ({ isModalOpen, setIsModalOpen }) => {
+const Modals: React.FC<ModalProps> = ({ isModalOpen, setIsModalOpen },modal) => {
   return (
     <div
       className={clsx({
@@ -13,15 +20,36 @@ const Modal: React.FC<ModalProps> = ({ isModalOpen, setIsModalOpen }) => {
         [classes.isModalOpen]: isModalOpen,
       })}
     >
-      <div className="modal">
-          <div className="modal-content">
-            <span className="close" onClick={()=>setIsModalOpen(false)}>Close</span>
-            <h2>Salom</h2>
-            <p>Lorem</p>
+        <button className={classes.closeBtn} onClick={() => setIsModalOpen(false)}>
+          <FaTimes className={classes.timesIcon} size={25} />
+        </button>
+      <div className={classes.imgItems}>
+        <img src={react} alt="react" />
+        <div className={classes.information}>
+          <h1>React</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut,
+            eveniet? Ad aliquam soluta excepturi veniam atque aperiam molestiae
+            asperiores expedita.
+          </p>
+          <h2>Project Stack</h2>
+          <ul>
+            <li>React</li>
+            <li>Redux+Saga+Toolkit</li>
+            <li>HTML/CSS</li>
+          </ul>
+          <ul>
+            <li>PHP</li>
+            <li>PostgreSQL</li>
+          </ul>
+          <div className={classes.btns}>
+          <button className={classes.viewProject}>View Project <HiArrowTopRightOnSquare className={classes.viewP} size={26}/> </button>
+          <button className={classes.viewProject}>Code <HiCode className={classes.viewP} size={26}/> </button>
           </div>
         </div>
+      </div>
     </div>
   );
 };
 
-export default Modal;
+export default Modals;
